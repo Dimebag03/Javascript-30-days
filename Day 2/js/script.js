13361add0b272fbce16397e647838986dfa922b2
emanoel <emanoel@viasoft.com.br>
2017-11-10 14:56:12 -0200

    Implementado o Clock Js

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -1,23 +1,26 @@
-const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+(() => {
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
 
-function setDate() {
-    const now = new Date;
-    const seconds = now.getSeconds();
-    const secondsDegree = ((seconds / 60) * 360) + 90;
+    function setDate() {
+        const now = new Date;
+        const seconds = now.getSeconds();
+        const secondsDegree = ((seconds / 60) * 360) + 90;
 
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
-    const min = now.getMinutes();
-    const minDegree = ((min / 60) * 360) + 90;
+        const min = now.getMinutes();
+        const minDegree = ((min / 60) * 360) + 90;
 
-    minHand.style.transform = `rotate(${minDegree}deg)`;
+        minHand.style.transform = `rotate(${minDegree}deg)`;
 
-    const hour = now.getHours();
-    const hourDegree = ((hour / 12) * 360) + 90;
+        const hour = now.getHours();
+        const hourDegree = ((hour / 12) * 360) + 90;
 
-    hourHand.style.transform = `rotate(${hourDegree}deg)`;
-}
+        hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    }
 
-setInterval(setDate, 1000);
+    setDate();
+    setInterval(setDate, 1000);
+})();
