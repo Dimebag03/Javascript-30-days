0bd7857f56bd785f6b2c76f3f1d8a23b200a09aa
emanoel <emanoel@viasoft.com.br>
2017-11-22 14:46:06 -0200

    Dia 5

---
+++ Day 5/js/script.js
@@ -0,0 +1,14 @@
+const panelElements = document.querySelectorAll('.panel');
+
+function setActive() {
+    panelElements.forEach(ele => ele === this ? this.classList.toggle('active') : ele.classList.remove('active'));
+}
+
+function setTranslate(e) {
+    if (e.propertyName.includes('flex')) {
+        this.querySelector('p').classList.toggle('active');
+    }
+}
+
+panelElements.forEach(ele => ele.addEventListener('click', setActive));
+panelElements.forEach(ele => ele.addEventListener('transitionend', setTranslate));
